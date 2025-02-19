78103089ff038d0749f0c743d94c01aa4ff11867
Aleksey-Smouzh <xmz@tut.by>
2025-02-19 18:42:01 +0100

    array

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MySql</title>
+</head>
+<body>
+    <?php
+    $hola = array("mam" => "pl", "bab" => "ru");
+    print_r ($hola);
+    echo $hola["mam"]
+    
+    ?>
+
+<h1>hi</h1>
+
+
+
+
+</body>
+</html>
